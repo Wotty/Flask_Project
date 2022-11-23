8281b0737cbf7fba86e4e8c7c6b1f963dbf0c1f8
Will <Wotty@users.noreply.github.com>
2022-11-23 13:18:29 +0000

    Completed project and Fixed bug not finding routing

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import sqlite3
 from werkzeug.exceptions import abort
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
@@ -12,27 +21,10 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
 
-if __name__ == '__main__':
-    app.run()
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
-
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
+@app.route('/post/<int:id>')
+def post(id):
+    post = get_post(id)
     return render_template('post.html', post=post)
 
 @app.route('/create', methods=('GET', 'POST'))
@@ -52,7 +44,7 @@ def create():
             return redirect(url_for('index'))
     return render_template('create.html')
 
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+@app.route('/edit/<int:id>', methods=('GET', 'POST'))
 def edit(id):
     post = get_post(id)
 
@@ -72,3 +64,13 @@ def edit(id):
             return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
